57a113ec1b7b9f0df8f95c60c17f14b1dc73af14
thanhphong39 <phongntse181679@fpt.edu.vn>
2026-05-07 14:32:35 +0700

    first commit

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: { port: 5174 },
-});
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] })
+  ],
+})
